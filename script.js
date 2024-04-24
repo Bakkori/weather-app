1fc4e3433fe4b42ad44000db28e16185682fe939
Abubaker Mohammed <97407222+Bakkori@users.noreply.github.com>
2024-04-24 22:31:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let button = document.getElementById('btn');
+let input = document.getElementById('city');
+let result = document.getElementById('result');
+let dgree = document.getElementById('dgree');
+
+const apiKey = "02438250de1148b1883211155242304";
+const apiUrl = "http://api.weatherapi.com/v1/forecast.json?key=02438250de1148b1883211155242304&q=Jeddah&days=1&aqi=no&alerts=no";
+
+button.addEventListener("click", function(){
+    let city = input.value;
+    result.innerText = "Getting result...";
+    getWeatherData(city);
+    /////////////////
+})
+
+async function getWeatherData(city){
+    let apiUrl = "http://api.weatherapi.com/v1/forecast.json?key=02438250de1148b1883211155242304&q="+city+"&days=1&aqi=no&alerts=no";
+    let response = await fetch(apiUrl);
+    let responseObj = await response.json();
+    console.log(responseObj.location);
+    result.innerText = "The temperature for "+city+" is: ";
+    let icon = '🙂';
+    if (responseObj.current.temp_c < 15) {
+        icon = '🥶';
+    } else if (responseObj.current.temp_c > 30) {
+        icon = '🥵';
+    }
+    dgree.innerText = responseObj.current.temp_c+"°c "+icon;
+}
+    
